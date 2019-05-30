15bb62cc90697e2b25470ff5c5fadcd9f28ef989
Andy5am <38666859+Andy5am@users.noreply.github.com>
2019-05-30 00:30:51 -0600

    agregar

---
+++ Assets/Standard_Assets/Characters/ThirdPersonCharacter/Scripts/AICharacterControl.cs
@@ -0,0 +1,56 @@
+using System;
+using UnityEngine;
+
+namespace UnityStandardAssets.Characters.ThirdPerson
+{
+    [RequireComponent(typeof (UnityEngine.AI.NavMeshAgent))]
+    [RequireComponent(typeof (ThirdPersonCharacter))]
+    public class AICharacterControl : MonoBehaviour
+    {
+        public UnityEngine.AI.NavMeshAgent agent { get; private set; }             // the navmesh agent required for the path finding
+        public ThirdPersonCharacter character { get; private set; } // the character we are controlling
+        public Transform target;                                    // target to aim for
+        private Vector3 distancia;
+        private float separacion;
+
+        private void Start()
+        {
+            // get the components on the object we need ( should not be null due to require component so no need to check )
+            agent = GetComponentInChildren<UnityEngine.AI.NavMeshAgent>();
+            character = GetComponent<ThirdPersonCharacter>();
+
+	        agent.updateRotation = false;
+	        agent.updatePosition = true;
+        }
+
+
+        private void Update()
+        {
+            distancia = (transform.position - target.transform.position);
+            distancia.y = 0;
+            separacion = distancia.magnitude;
+            distancia /= separacion;
+
+            if (separacion < 5) { 
+
+            if (target != null)
+                agent.SetDestination(target.position);
+
+            if (agent.remainingDistance > agent.stoppingDistance)
+                character.Move(agent.desiredVelocity, false, false);
+
+            }
+            else
+            { 
+                character.Move(Vector3.zero, false, false);
+            }
+
+        }
+
+
+        public void SetTarget(Transform target)
+        {
+            this.target = target;
+        }
+    }
+}
