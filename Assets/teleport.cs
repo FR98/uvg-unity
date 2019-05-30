0231ad6cdd517e9ee023aa34dcd020b26e3b038e
Andy5am <38666859+Andy5am@users.noreply.github.com>
2019-05-30 00:00:01 -0600

    avance

---
+++ Assets/teleport.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class teleport : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerStay(Collider collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            changeScene("SampleScene1");
+        }
+    }
+
+    public void changeScene(string newScene)
+    {
+        SceneManager.LoadScene(newScene);
+    }
+}
